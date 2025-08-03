d3d80f823d407cc1cf8f64673663dbad64461bd1
“Musician” <hhajriwnko@gmail.com>
2025-08-03 22:44:37 +0530

    Add models for comments, likes, playlists, and tweets

---
+++ src/models/playlist.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema({
+  name: {
+    type: String,
+    requred: true
+  },
+  discription: {
+    type: String
+  },
+  videos: {
+    type: Schema.Types.ObjectId,
+    ref: "Video",
+    required: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+},{timestamps: true});
+
+export const Playlist = mongoose.model('Playlist', playlistSchema);
